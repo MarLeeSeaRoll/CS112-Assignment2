f0805630643cec52972163e17c5801f12871ff22
Rohan Nandan <rohannandan2004@gmail.com>
2024-10-16 18:37:50 +1200

    Fix: Improving Update Marks for main.cpp

---
+++ Messages.h
@@ -0,0 +1,46 @@
+#ifndef MESSAGES_H
+#define MESSAGES_H
+
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <iostream>
+
+using namespace  std;
+
+void printWelcomeMessage()
+{
+    cout << "+-------------------------------------------------+\n";
+    cout << "|      WELCOME TO THE STUDENT GRADES PROGRAM      |\n";
+    cout << "+-------------------------------------------------+\n";
+}
+
+void displayMenu(){
+        cout << "\nMenu:\n";
+        cout << "1. Print All Students\n";
+        cout << "2. Update Student Marks\n";
+        cout << "3. Show Highest Scorer\n";
+        cout << "4. Calculate Average Marks\n";
+        cout << "5. Calculate Pass Rate\n";
+        cout << "6. Exit\n";
+        cout << "Enter your choice: ";
+}
+
+void endProgram(bool& continueExecuting)
+{
+    continueExecuting = false;
+
+    cout << endl;
+    cout << "+-------------------------------------+\n";
+    cout << "|  Thank you for using this program!  |\n";
+    cout << "+-------------------------------------+\n";
+}
+void updateMarkChoices(){
+    cout << "Which Mark Would You Like To Update?" << endl;
+    cout << "1. Coursework Only" << endl;
+    cout << "2. Final Exam Only" << endl;
+    cout << "3. Course and Final Exam" << endl;	
+}
+	
+
+#endif
--- Utilities.h
+++ Utilities.h
@@ -8,11 +8,36 @@
 #include "Student.h"
 
 // Function Prototypes
+void programStartQuit(bool& continueExecuting);
+
 void readFileAndStoreData(LinkedList<Student>& studentList, const string& filename);
 
 double calculateAverageMarks(const LinkedList<Student>& studentList);
 
 Student* getHighestScorer(LinkedList<Student>& studentList);
+
+void programStartQuit(bool& continueExecuting)
+{
+    string userDecision;
+    cout << "Enter S to start or Q to quit" << endl;
+    cin >> userDecision;
+    
+    while(userDecision != "S" && userDecision != "s" && userDecision != "Q" && userDecision != "q") {
+        cin.clear();
+        cout << "ERROR! Invalid input detected." << endl;
+        cout << "Please enter either \"S\" or \"Q\" only." << endl;
+        cin >> userDecision;
+    }
+    
+    if (userDecision == "S" || userDecision == "s")
+	{
+		continueExecuting = true;
+	}
+	else if (userDecision == "Q" || userDecision == "q")
+	{
+		continueExecuting = false;
+	}
+}
 
 // Function Definitions
 void readFileAndStoreData(LinkedList<Student>& studentList, const string& filename) {
@@ -37,7 +62,7 @@ double calculateAverageMarks(const LinkedList<Student>& studentList) {
     int count = 0;
 
     Node<Student>* current = studentList.getHead();
-    while (current != nullptr) {
+    while (current != NULL) {
         totalMarks += current->data.getTotalMarks();
         count++;
         current = current->next;
@@ -47,7 +72,10 @@ double calculateAverageMarks(const LinkedList<Student>& studentList) {
 }
 
 Student* getHighestScorer(LinkedList<Student>& studentList) {
-    if (!studentList.getHead()) return nullptr;
+    if (!studentList.getHead())
+    {
+        return NULL;
+    }
 
     Node<Student>* current = studentList.getHead();
     Student* topStudent = &(current->data);
@@ -61,5 +89,33 @@ Student* getHighestScorer(LinkedList<Student>& studentList) {
 
     return topStudent;
 }
+
+// Valid input for updating marks
+    template <typename V>
+    V ValidateUpdateMarks(V lowerLimit, V upperLimit)
+    {
+        V input;
+        cin >> input;
+    
+        while(input < lowerLimit || input > upperLimit || cin.fail())
+        {
+            if (cin.fail())
+            {
+                cin.clear();
+                string dummy;
+                cin >> dummy;
+                cout << "ERROR! Invalid option detected." << endl;
+                cout << "Please enter a number." << endl;
+            }
+            else if (input < lowerLimit || input > upperLimit)
+            {
+                cin.clear();
+                cout << "ERROR! Your choice is not in range." << endl;
+                cout << "Please enter a number between " << lowerLimit << " and " << upperLimit << endl;
+            }
+            cin >> input;
+        }
+        return input;
+    }
 
 #endif
